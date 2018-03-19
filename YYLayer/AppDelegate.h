dec2632aeb9cc930000c83ced2e77484c9712323
yunhai0417 <yunhai.wu@nestia.com>
2018-03-19 22:12:29 +0800

    添加基本文件

---
+++ YYLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYLayer
+//
+//  Created by 吴云海 on 2018/3/19.
+//  Copyright © 2018年 吴云海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYLayer
+//
+//  Created by 吴云海 on 2018/3/19.
+//  Copyright © 2018年 吴云海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
