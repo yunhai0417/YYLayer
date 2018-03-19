f79e0de33ea86c366a72d7c327fd8ae68ced03a2
yunhai0417 <yunhai.wu@nestia.com>
2018-03-19 17:45:06 +0800

    添加默认工程

---
+++ YYLayer/YYLayer/YYViewController.m
@@ -0,0 +1,40 @@
+//
+//  YYViewController.m
+//  YYLayer
+//
+//  Created by 吴云海 on 2018/3/19.
+//  Copyright © 2018年 吴云海. All rights reserved.
+//
+
+#import "YYViewController.h"
+
+@interface YYViewController ()
+
+@end
+
+@implementation YYViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)buttonAction:(id)sender {
+    NSLog(@"xxxxx");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
